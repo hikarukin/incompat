6e4d1f57cc54388cc34240ca08982b028c70877f
hikarukin <156204784+hikarukin@users.noreply.github.com>
2025-01-20 09:52:05 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hikarukin/incompat
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+package main
+
+import "fmt"
+
+var Version string = "test"
+
+func main() {
+	fmt.Printf("Version: %s", Version)
+}
